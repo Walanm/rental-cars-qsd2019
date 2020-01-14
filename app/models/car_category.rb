cee6834ad9bdadb62558bc7e9e5d3783be15452e
Walan Marcel Teles Oliveira <walan.dn@gmail.com>
2020-01-13 22:53:31 -0300

    Adiciona validações numéricas em Car Category

--- app/models/car_category.rb
+++ app/models/car_category.rb
@@ -1,7 +1,12 @@
 class CarCategory < ApplicationRecord
   has_many :car_models
   validates :name, presence: { message: 'Nome não pode ficar em branco'}
-  validates :daily_rate, presence: { message: 'Taxa Diária não pode ficar em branco'}
-  validates :car_insurance, presence: { message: 'Seguro do Carro não pode ficar em branco'}
-  validates :third_party_insurance, presence: { message: 'Seguro contra Terceiros não pode ficar em branco'}
+  validates :daily_rate, presence: { message: 'Taxa Diária não pode ficar em branco'},
+                         numericality: { greater_than: 0, message: 'Taxa Diária deve ser maior que zero' }
+
+  validates :car_insurance, presence: { message: 'Seguro do Carro não pode ficar em branco'},
+                            numericality: { greater_than: 0,  message: 'Seguro do Carro deve ser maior que zero' }
+                              
+  validates :third_party_insurance, presence: { message: 'Seguro contra Terceiros não pode ficar em branco'},
+                                    numericality: { greater_than: 0, message: 'Seguro contra Terceiros deve ser maior que zero' }
 end
--- spec/features/admin_register_car_category_spec.rb
+++ spec/features/admin_register_car_category_spec.rb
@@ -36,4 +36,21 @@ feature 'Admin register car category' do
     expect(page).to have_content('Seguro do Carro não pode ficar em branco')
     expect(page).to have_content('Seguro contra Terceiros não pode ficar em branco')
   end
+
+  scenario 'and values must be greater than zero' do
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Registrar nova categoria'
+
+    fill_in 'Nome', with: 'A'
+    fill_in 'Taxa Diária', with: '-1.0'
+    fill_in 'Seguro do Carro', with: '-2.5'
+    fill_in 'Seguro contra Terceiros', with: '-3.7'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Você deve corrigir os seguintes erros para continuar')
+    expect(page).to have_content('Taxa Diária deve ser maior que zero')
+    expect(page).to have_content('Seguro do Carro deve ser maior que zero')
+    expect(page).to have_content('Seguro contra Terceiros deve ser maior que zero')
+  end
 end
