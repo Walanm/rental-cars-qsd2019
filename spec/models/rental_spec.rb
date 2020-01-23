8e311893af5d8ca386d3d8fad24e7749b837a8f6
Walan Marcel Teles Oliveira <walan.dn@gmail.com>
2020-01-23 07:48:38 -0300

    Corrige testes unitários para datas de Agendar Locação

--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -4,13 +4,18 @@ describe Rental do
   describe '#start_date_cannot_be_in_the_past' do
     it 'validates successfully' do
       rental = Rental.new(start_date: 1.day.ago, end_date: 10.days.from_now)
+
       rental.valid?
+
       expect(rental.errors[:start_date]).to include('Data de início não pode ser no passado')
     end
 
-    it 'validates presence' do
+    it 'doesnt have start date' do
       rental = Rental.new(start_date: nil, end_date: Date.current)
+
       rental.valid?
+
+      expect(rental.errors[:start_date]).not_to include('Data de início não pode ser no passado')
       expect(rental.errors[:start_date]).to include('Data de Início não pode ficar em branco')
     end
   end
@@ -18,14 +23,28 @@ describe Rental do
   describe '#end_date_greater_than_start_date' do
     it 'validates successfully' do
       rental = Rental.new(start_date: 1.day.from_now, end_date: 1.day.ago)
+
       rental.valid?
+
       expect(rental.errors[:end_date]).to include('Data de término deve ser após data de início')
     end
 
-    it 'validates presence' do
+    it 'doesnt have end date' do
       rental = Rental.new(start_date: Date.current, end_date: nil)
+
       rental.valid?
+
+      expect(rental.errors[:end_date]).not_to include('Data de término deve ser após data de início')
       expect(rental.errors[:end_date]).to include('Data de Término não pode ficar em branco')
+    end
+
+    it 'doesnt have start date' do
+      rental = Rental.new(start_date: nil, end_date: Date.current)
+
+      rental.valid?
+
+      expect(rental.errors[:end_date]).not_to include('Data de término deve ser após data de início')
+      expect(rental.errors[:start_date]).to include('Data de Início não pode ficar em branco')
     end
   end
 end
