df35e52257df2967d855542aabb59324fd328b37
Walan Marcel Teles Oliveira <walan.dn@gmail.com>
2020-01-28 20:48:17 -0300

    Atualiza testes unitários de car rental

--- spec/models/car_rental_spec.rb
+++ spec/models/car_rental_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 describe CarRental do
-  describe '#validates' do
+  describe 'validates' do
     it 'daily_rate must exist' do
-      car_rental = CarRental.new
+      car = Car.new(license_plate: 'NVN1010')
+      rental = Rental.new(code: 'XFB0000')
+      car_rental = CarRental.new(car: car, rental: rental, daily_rate: nil,
+                                 car_insurance: 90.5, third_party_insurance: 100.1,
+                                 start_mileage: 200)
 
       car_rental.valid?
 
@@ -11,15 +15,22 @@ describe CarRental do
     end
 
     it 'car_insurance must exist' do
-      car_rental = CarRental.new
-
+      car = Car.new(license_plate: 'NVN1010')
+      rental = Rental.new(code: 'XFB0000')
+      car_rental = CarRental.new(car: car, rental: rental, daily_rate: 90.5,
+                                 car_insurance: nil, third_party_insurance: 100.1,
+                                 start_mileage: 200)
       car_rental.valid?
 
       expect(car_rental.errors[:car_insurance]).to include('Seguro do Carro não pode ficar em branco')
     end
 
     it 'third_party_insurance must exist' do
-      car_rental = CarRental.new
+      car = Car.new(license_plate: 'NVN1010')
+      rental = Rental.new(code: 'XFB0000')
+      car_rental = CarRental.new(car: car, rental: rental, daily_rate: 90.5,
+                                 car_insurance: 100.1, third_party_insurance: nil,
+                                 start_mileage: 200)
 
       car_rental.valid?
 
@@ -27,7 +38,11 @@ describe CarRental do
     end
 
     it 'start_mileage must exist' do
-      car_rental = CarRental.new
+      car = Car.new(license_plate: 'NVN1010')
+      rental = Rental.new(code: 'XFB0000')
+      car_rental = CarRental.new(car: car, rental: rental, daily_rate: 90.5,
+                                 car_insurance: 190.8, third_party_insurance: 100.1,
+                                 start_mileage: nil)
 
       car_rental.valid?
 
